32c315898419f7d0256a43acb21382043a078398
Harald Wallin <Hara.w@hotmail.com>
2026-09-24 17:23:44 +0200

    feat: load articles from API

--- client/src/pages/ArticlesPage.tsx
+++ client/src/pages/ArticlesPage.tsx
@@ -1,43 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { apiFetch } from "../lib/api";
 
 import ArticleCard from "../components/articles/articleCard";
 import ArticleModal from "../components/articles/articleModal";
 
 import type { Article } from "../types/article";
 
-//TEST (sorry not sorry Sheema)
-const testArticles: Article[] = [
-  {
-    id: "1",
-    article_title: "Så skriver du ett bra CV",
-    article_description: "Fem enkla tips för att förbättra ditt CV.",
-    article_text: "Här kommer hela artikeltexten senare.",
-    required_subscription_level_id: 1,
-    created_by: null,
-    created_at: "2026-09-22",
-    updated_at: "2026-09-22",
-  },
-  {
-    id: "2",
-    article_title: "Förbered dig inför arbetsintervjun",
-    article_description: "Så känner du dig mer förberedd inför intervjun.",
-    article_text: "Här kommer hela artikeltexten senare.",
-    required_subscription_level_id: 2,
-    created_by: null,
-    created_at: "2026-09-22",
-    updated_at: "2026-09-22",
-  },
-];
+
 
 function ArticlesPage(){
 
   const [isArticleModalOpen, setIsArticleModalOpen] = useState(false);
 
+  //innehåller en !array av Article!
+  const [articles, setArticles] = useState<Article[]> ([]);
+
+
+  useEffect(() =>{
+    async function loadArticles(){
+      try{
+        const data = await apiFetch<Article[]>('/api/articles');
+        setArticles(data);
+      }catch (error){
+        console.error('Kunde inte hämta artiklar:', error);
+      };
+    };
+
+    loadArticles();
+  }, []);
+
   return(
     <main className='container'>
       <h1>Artiklar</h1>
 
-      {testArticles.map((article) =>(
+      {articles.map((article) =>(
         <ArticleCard key={article.id} article={article} />
       ))}
 
