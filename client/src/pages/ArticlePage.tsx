1434845a0519ec2baf3d11a88dae870a8f040452
Harald Wallin <Hara.w@hotmail.com>
2026-09-24 22:15:34 +0200

    feat: implemented better error-handling

--- client/src/pages/ArticlePage.tsx
+++ client/src/pages/ArticlePage.tsx
@@ -10,31 +10,65 @@ function ArticlePage() {
   const {id} = useParams();
   const [article, setArticle] = useState <Article | null> (null);
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
   useEffect(() => {
     async function loadArticle(){
-      if (!id) {
+
+      setIsLoading(true);
+      setErrorMessage('');
+      setArticle(null);
+
+      if (!id){
+        setErrorMessage('Artikel-ID saknas!')
+        setIsLoading(false)
         return;
-      }
+      };
 
       try{
         const data = await apiFetch<Article>(`/api/articles/${id}`);
         setArticle(data);
 
       } catch (error) {
+
         console.error('Kunde inte hämta artikeln:', error);
 
-      }
+        setErrorMessage(error instanceof Error
+          ? error.message
+          : 'Kunde inte hämta artikeln'
+        );
+
+      }finally{
+        setIsLoading(false)
+      };
     };
 
     loadArticle();
   }, [id]);
 
-  if (!article){
+  if (isLoading){
     return (
       <main className='container'>
         <p>Laddar artikeln...</p>
       </main>
     );
+  };
+
+  if(errorMessage){
+    return (
+      <main className='container'>
+        <p>{errorMessage}</p>
+      </main>
+    );
+  };
+
+  if(!article){
+    return(
+      <main className='container'>
+        <p>Artikeln kunde inte hittas</p>
+      </main>
+    )
   };
 
   return(
